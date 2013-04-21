61528507c2d6f37757ec83ef8711b473149de6d8
jens.ritter@gmail.com <jens.ritter@gmail.com@45685e78-59ec-8729-ecae-d2fce6821bd3>
2013-04-21 03:22:01 +0000

    allow to add a single File/url/magnetlink. (also updated Versions of dependencies)

--- trunk/src/test/java/ca/benow/transmission/model/Example.java
+++ trunk/src/test/java/ca/benow/transmission/model/Example.java
@@ -11,11 +11,13 @@ public class Example {
   /**
    * @param args
    */
-  public static void main(
-      String[] args) {
+  public static void main(String[] args) {
     try {
       TransmissionClient client = new TransmissionClient(new URL("http://hacker:linux@192.168.100.1:9091/transmission/rpc"));
       List<TorrentStatus> torrents = client.getAllTorrents(new TorrentField[] { TorrentField.doneDate,TorrentField.id, TorrentField.dateCreated, TorrentField.name});
+
+      client.addTorrent("magnet:?xt=urn:btih:06af168404029a8ca21a06c43fcb7dd49c288c4f&dn=debian-6.0.7-amd64-CD-1.iso&tr=http%3A%2F%2Fbttracker.debian.org%3A6969%2Fannounce");
+      
       for (TorrentStatus curr : torrents) {
           if (curr.getId() == 28 || curr.getId() == 2) {
               //System.out.println(curr.getField(TorrentField.doneDate));
