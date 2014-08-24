3798f95b4ad05954b3af219521e8e34209a30984
jens.ritter@gmail.com <jens.ritter@gmail.com@45685e78-59ec-8729-ecae-d2fce6821bd3>
2014-08-24 10:35:24 +0000

    Handing of DuplicateTorrents

--- src/main/java/ca/benow/transmission/model/AddedTorrentInfo.java
+++ src/main/java/ca/benow/transmission/model/AddedTorrentInfo.java
@@ -1,38 +1,36 @@
 package ca.benow.transmission.model;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.json.*;
+import org.slf4j.*;
 
 public class AddedTorrentInfo extends JSONAccessor {
 
     private static Logger logger = LoggerFactory.getLogger(AddedTorrentInfo.class);
 
-  public AddedTorrentInfo(JSONObject jsonObject) {
-    super(jsonObject);
-  }
-
-  public int getId() throws JSONException {
-    return obj.getInt("id");
-  }
-
-  public String getName() throws JSONException {
-    return obj.getString("name");
-  }
-
-  public String getHashString() throws JSONException {
-    return obj.getString("hashString");
-  }
-
-  @Override
-  public String toString() {
-    try {
-        return obj.toString(2);
-    } catch (JSONException e) {
-        logger.error(e.getMessage(),e);
-        return e.getMessage();
+    public AddedTorrentInfo(JSONObject jsonObject) {
+        super(jsonObject);
     }
 
-  }
+    public int getId() throws JSONException {
+        return obj.getInt("id");
+    }
+
+    public String getName() throws JSONException {
+        return obj.getString("name");
+    }
+
+    public String getHashString() throws JSONException {
+        return obj.getString("hashString");
+    }
+
+    @Override
+    public String toString() {
+        try {
+            return obj.toString(2);
+        } catch (JSONException e) {
+            logger.error(e.getMessage(),e);
+            return e.getMessage();
+        }
+
+    }
 }
